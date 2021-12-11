d80cea3983f2bcabf850c76cba652efed57c6ff2
ssama1 <sriharsha.samanthula@nike.com>
2021-12-10 22:13:54 -0800

    java bad format test

--- src/main/java/com/comp/demo/DemoApplication.java
+++ src/main/java/com/comp/demo/DemoApplication.java
@@ -2,12 +2,15 @@ package com.comp.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+			System.out.println("test");
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
